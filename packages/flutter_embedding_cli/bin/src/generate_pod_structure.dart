7f3e3045d1f80536b707e92e326c0c5d81886d26
Kris Pypen <kris.pypen@gmail.com>
2026-08-10 18:09:34 +0200

    fix: dependency version mismatch

--- packages/flutter_embedding_cli/bin/src/generate_pod_structure.dart
+++ packages/flutter_embedding_cli/bin/src/generate_pod_structure.dart
@@ -122,7 +122,7 @@ Future<void> generatePodHelper(
       if (podName == 'SwiftProtobuf') {
         continue; // Skip SwiftProtobuf - it comes through FlutterEmbeddingModule dependencies
       }
-      writeStream.write('  s.dependency "$podName"\n');
+      writeStream.write('  s.dependency "$podName", "${podFilelock.getPodVersion(podName)}"\n');
     }
   }
 
